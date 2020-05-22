ffa464ae14fe02e5382be38b0b3eb40c7c8a3cba
robdan89 <fuquardwork@gmail.com>
2020-05-22 14:24:16 -0500

    place-names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,29 @@
 function main() {
   // Put your code here
+  console.log("All Place Names");
 
+  const placeNames = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China",
+  ];
+  for (let name of placeNames) {
+    console.log(name);
+  }
+
+  console.log("");
+
+  console.log("'The' Place Names");
+  for (let name of placeNames) {
+    if (name.startsWith("The")) {
+      console.log(name);
+    }
+  }
 }
 
 main();
